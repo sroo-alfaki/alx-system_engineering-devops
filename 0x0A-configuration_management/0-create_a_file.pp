d69b15859713334ba65c7a9d4e240d10aab44757
saria <srooalfakiahmed@gmail.com>
2024-05-03 19:08:43 -0700

    aa

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 #Using Puppet, create a file in /tmp
 file { '/tmp/school':
-	ensure  => file,
-		content => 'I love Puppet',
-		mode    => '0744',
-		owner   => 'www-data',
-		group   => 'www-data',
+	content => 'I love Puppet',
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
 }
